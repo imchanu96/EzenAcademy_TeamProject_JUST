ba172f8c5c9c534ccbe21c7264f6ba5eebfa08ea
miyoung93 <miyoung93@hanmail.net>
2023-09-11 15:11:23 +0900

    팀 프로젝트 해야 할 일  - Spring 프로젝트 수정  - DB 설계  - jsp 작성
    팀 프로젝트 적용한 일
     - Spring 프로젝트 수정
     - DB 설계
    
    내가 맡은 일
     - Spring 프로젝트 수정
     - jsp 작성
    
    내가 적용한 일
     - Spring 프로젝트 수정
     - jsp 작성(진행 중)
    
    작성자: PMY

--- JobHub/src/main/java/com/jobhub/personal/dto/PersonalMemberDto.java
+++ JobHub/src/main/java/com/jobhub/personal/dto/PersonalMemberDto.java
@@ -1,5 +1,20 @@
 package com.jobhub.personal.dto;
 
+import java.util.Date;
+
 public class PersonalMemberDto {
 
+	private int pNo;
+	private String pName;
+	private String pId;
+	private String pPwd;
+	private String pPhoneNum;
+	private String pCom;
+	private String pNickname;
+	private String pProfile;
+	private String pEmail;
+	private Date pHireDate;
+	private String pKeyword;
+	private String pTemporarily;
+	
 }
