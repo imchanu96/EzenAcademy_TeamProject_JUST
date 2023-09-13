58dcdcbab35b62bd1584fbf6901d4999f4de046c
Jihoon Park <jihoon.park0128@gmail.com>
2023-09-13 14:31:53 +0900

    팀 프로젝트 해야 할 일  - Spring 프로젝트 수정  - jsp 작성, 업데이트
    팀 프로젝트 적용한 일
     - Spring 프로젝트 수정
    
    내가 맡은 일
     - jsp 작성, 업데이트
     - 기업회원 로그인 페이지 연결
    
    내가 적용한 일
     - 기업회원 로그인 페이지 연결
    
    작성자: PJH

--- JobHub/src/main/java/com/jobhub/company/controller/CompanyMemberController.java
+++ JobHub/src/main/java/com/jobhub/company/controller/CompanyMemberController.java
@@ -1,5 +1,32 @@
 package com.jobhub.company.controller;
 
+import javax.servlet.http.HttpSession;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.jobhub.company.service.CompanyMemberService;
+import com.jobhub.company.controller.CompanyMemberController;
+
+@Controller
 public class CompanyMemberController {
+	
+	private static final Logger log 
+	= LoggerFactory.getLogger(CompanyMemberController.class);
+	
+	@Autowired
+	private CompanyMemberService companyMemberService;
+	
+	@RequestMapping(value = "/company/login.do", method = RequestMethod.GET)
+	public String login(HttpSession session, Model model) {
+		log.info("Welcome CompanyController login!");
+		
+		return "company/auth/CompanyLoginForm";
+	}
 
 }
--- JobHub/src/main/java/com/jobhub/company/service/CompanyMemberService.java
+++ JobHub/src/main/java/com/jobhub/company/service/CompanyMemberService.java
@@ -1,5 +1,9 @@
 package com.jobhub.company.service;
 
+import com.jobhub.company.dto.CompanyMemberDto;
+
 public interface CompanyMemberService {
 
+	CompanyMemberDto companyMemberExist(String cCode, String cPwd);
+	
 }
