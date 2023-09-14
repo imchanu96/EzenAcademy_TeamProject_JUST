045dc1229962cf19533516cdc1871537440a234d
Imchanu <cksdn9979@gmail.com>
2023-09-14 17:47:52 +0900

    오류 수정완료

--- JobHub/src/main/java/com/jobhub/personal/dao/PersonalMemberDao.java
+++ JobHub/src/main/java/com/jobhub/personal/dao/PersonalMemberDao.java
@@ -2,15 +2,15 @@ package com.jobhub.personal.dao;
 
 import java.util.List;
 
-import com.jobhub.admin.dto.AdminDto;
 import com.jobhub.personal.dto.PersonalMemberDto;
 
 public interface PersonalMemberDao {
 	
-	PersonalMemberDto personalMemberExist(String pId, String pPwd);
+	public PersonalMemberDto personalMemberExist(String pId, String pPwd);
 
 	public List<PersonalMemberDto> memberSelectList();
 
 	public int memberInsertOne(PersonalMemberDto personalMemberDto);
+	
 	
 }
