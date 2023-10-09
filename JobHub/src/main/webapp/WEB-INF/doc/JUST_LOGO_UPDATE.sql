1b082b1867bf1bcdf21f5692365328eeadf48738
Jihoon Park <jihoon.park0128@gmail.com>
2023-10-09 14:32:18 +0900

    2023-10-09 by PJH
    더미 로고 업데이트

---
+++ JobHub/src/main/webapp/WEB-INF/doc/JUST_LOGO_UPDATE.sql
@@ -0,0 +1,331 @@
+SELECT * FROM COMPANY;
+
+UPDATE COMPANY
+SET COMPANY_LOGO = '/JobHub/resources/img/logo/samElec.svg'
+where company_number = 1;
+
+UPDATE COMPANY
+SET COMPANY_LOGO = '/JobHub/resources/img/logo/hmm.svg'
+where company_number = 2;
+
+UPDATE COMPANY
+SET COMPANY_LOGO = '/JobHub/resources/img/logo/hmotors.svg'
+where company_number = 3;
+
+UPDATE COMPANY
+SET COMPANY_LOGO = '/JobHub/resources/img/logo/lgchem.svg'
+where company_number = 4;
+
+UPDATE COMPANY
+SET COMPANY_LOGO = '/JobHub/resources/img/logo/skhy.svg'
+where company_number = 5;
+
+UPDATE COMPANY
+SET COMPANY_LOGO = '/JobHub/resources/img/logo/ibk.svg'
+where company_number = 6;
+
+UPDATE COMPANY
+SET COMPANY_LOGO = '/JobHub/resources/img/logo/soil.svg'
+where company_number = 7;
+
+UPDATE COMPANY
+SET COMPANY_LOGO = '/JobHub/resources/img/logo/kia.jpg'
+where company_number = 8;
+
+UPDATE COMPANY
+SET COMPANY_LOGO = '/JobHub/resources/img/logo/kal.jpg'
+where company_number = 9;
+
+UPDATE COMPANY
+SET COMPANY_LOGO = '/JobHub/resources/img/logo/kb.svg'
+where company_number = 10;
+
+UPDATE COMPANY
+SET COMPANY_LOGO = '/JobHub/resources/img/logo/kakao.svg'
+where company_number = 11;
+
+UPDATE COMPANY
+SET COMPANY_LOGO = '/JobHub/resources/img/logo/mobis.svg'
+where company_number = 12;
+
+UPDATE COMPANY
+SET COMPANY_LOGO = '/JobHub/resources/img/logo/samct.svg'
+where company_number = 13;
+
+UPDATE COMPANY
+SET COMPANY_LOGO = '/JobHub/resources/img/logo/shinhan.svg'
+where company_number = 14;
+
+UPDATE COMPANY
+SET COMPANY_LOGO = '/JobHub/resources/img/logo/glovis.svg'
+where company_number = 15;
+
+UPDATE COMPANY
+SET COMPANY_LOGO = '/JobHub/resources/img/logo/woori.svg'
+where company_number = 16;
+
+UPDATE COMPANY
+SET COMPANY_LOGO = '/JobHub/resources/img/logo/hana.svg'
+where company_number = 17;
+
+UPDATE COMPANY
+SET COMPANY_LOGO = '/JobHub/resources/img/logo/samsins.svg'
+where company_number = 18;
+
+UPDATE COMPANY
+SET COMPANY_LOGO = '/JobHub/resources/img/logo/naver.svg'
+where company_number = 19;
+
+UPDATE COMPANY
+SET COMPANY_LOGO = '/JobHub/resources/img/logo/hanhwa.svg'
+where company_number = 20;
+
+commit;
+
+
+
+UPDATE COMPANY
+SET COMPANY_LOGO = '/JobHub/resources/img/logo/emart.png'
+where company_number = 21;
+
+UPDATE COMPANY
+SET COMPANY_LOGO = '/JobHub/resources/img/logo/dbins.jpg'
+where company_number = 22;
+
+UPDATE COMPANY
+SET COMPANY_LOGO = '/JobHub/resources/img/logo/hyundaisteel.jpg'
+where company_number = 23;
+
+UPDATE COMPANY
+SET COMPANY_LOGO = '/JobHub/resources/img/logo/ktng.png'
+where company_number = 24;
+
+UPDATE COMPANY
+SET COMPANY_LOGO = '/JobHub/resources/img/logo/sdbio.png'
+where company_number = 25;
+
+UPDATE COMPANY
+SET COMPANY_LOGO = '/JobHub/resources/img/logo/sktel.png'
+where company_number = 26;
+
+UPDATE COMPANY
+SET COMPANY_LOGO = '/JobHub/resources/img/logo/samsdi.png'
+where company_number = 27;
+
+UPDATE COMPANY
+SET COMPANY_LOGO = '/JobHub/resources/img/logo/lgelec.jpg'
+where company_number = 28;
+
+UPDATE COMPANY
+SET COMPANY_LOGO = '/JobHub/resources/img/logo/kogas.png'
+where company_number = 29;
+
+UPDATE COMPANY
+SET COMPANY_LOGO = '/JobHub/resources/img/logo/kt.png'
+where company_number = 30;
+
+UPDATE COMPANY
+SET COMPANY_LOGO = '/JobHub/resources/img/logo/lg.png'
+where company_number = 31;
+
+UPDATE COMPANY
+SET COMPANY_LOGO = '/JobHub/resources/img/logo/lginno.png'
+where company_number = 32;
+
+UPDATE COMPANY
+SET COMPANY_LOGO = '/JobHub/resources/img/logo/smasem.jpg'
+where company_number = 33;
+
+UPDATE COMPANY
+SET COMPANY_LOGO = '/JobHub/resources/img/logo/sambio.jpg'
+where company_number = 34;
+
+UPDATE COMPANY
+SET COMPANY_LOGO = '/JobHub/resources/img/logo/panocean.png'
+where company_number = 35;
+
+UPDATE COMPANY
+SET COMPANY_LOGO = '/JobHub/resources/img/logo/koinvest.jpg'
+where company_number = 36;
+
+UPDATE COMPANY
+SET COMPANY_LOGO = '/JobHub/resources/img/logo/lguplus.png'
+where company_number = 37;
+
+UPDATE COMPANY
+SET COMPANY_LOGO = '/JobHub/resources/img/logo/kozinc.jpg'
+where company_number = 38;
+
+UPDATE COMPANY
+SET COMPANY_LOGO = '/JobHub/resources/img/logo/samcard.png'
+where company_number = 39;
+
+UPDATE COMPANY
+SET COMPANY_LOGO = '/JobHub/resources/img/logo/samlife.png'
+where company_number = 40;
+
+COMMIT;
+
+UPDATE COMPANY
+SET COMPANY_LOGO = '/JobHub/resources/img/logo/khpet.svg'
+where company_number = 41;
+
+UPDATE COMPANY
+SET COMPANY_LOGO = '/JobHub/resources/img/logo/hdhs.svg'
+where company_number = 42;
+
+UPDATE COMPANY
+SET COMPANY_LOGO = '/JobHub/resources/img/logo/dbhi.webp'
+where company_number = 43;
+
+UPDATE COMPANY
+SET COMPANY_LOGO = '/JobHub/resources/img/logo/skt.svg'
+where company_number = 44;
+
+UPDATE COMPANY
+SET COMPANY_LOGO = '/JobHub/resources/img/logo/samsds.webp'
+where company_number = 45;
+
+UPDATE COMPANY
+SET COMPANY_LOGO = '/JobHub/resources/img/logo/lxint.svg'
+where company_number = 46;
+
+UPDATE COMPANY
+SET COMPANY_LOGO = '/JobHub/resources/img/logo/kgsteel.svg'
+where company_number = 47;
+
+UPDATE COMPANY
+SET COMPANY_LOGO = '/JobHub/resources/img/logo/celltrion.svg'
+where company_number = 48;
+
+UPDATE COMPANY
+SET COMPANY_LOGO = '/JobHub/resources/img/logo/kiwoom.svg'
+where company_number = 49;
+
+UPDATE COMPANY
+SET COMPANY_LOGO = '/JobHub/resources/img/logo/krafton.svg'
+where company_number = 50;
+
+UPDATE COMPANY
+SET COMPANY_LOGO = '/JobHub/resources/img/logo/nc.svg'
+where company_number = 51;
+
+UPDATE COMPANY
+SET COMPANY_LOGO = '/JobHub/resources/img/logo/skinn.svg'
+where company_number = 52;
+
+UPDATE COMPANY
+SET COMPANY_LOGO = '/JobHub/resources/img/logo/ls.svg'
+where company_number = 53;
+
+UPDATE COMPANY
+SET COMPANY_LOGO = '/JobHub/resources/img/logo/gsretail.svg'
+where company_number = 54;
+
+UPDATE COMPANY
+SET COMPANY_LOGO = '/JobHub/resources/img/logo/sksquare.svg'
+where company_number = 55;
+
+UPDATE COMPANY
+SET COMPANY_LOGO = '/JobHub/resources/img/logo/mirae.svg'
+where company_number = 56;
+
+UPDATE COMPANY
+SET COMPANY_LOGO = '/JobHub/resources/img/logo/lottefine.svg'
+where company_number = 57;
+
+UPDATE COMPANY
+SET COMPANY_LOGO = '/JobHub/resources/img/logo/yg.svg'
+where company_number = 58;
+
+UPDATE COMPANY
+SET COMPANY_LOGO = '/JobHub/resources/img/logo/hanjinkal.svg'
+where company_number = 59;
+
+UPDATE COMPANY
+SET COMPANY_LOGO = '/JobHub/resources/img/logo/sment.svg'
+where company_number = 60;
+
+commit;
+
+UPDATE COMPANY
+SET COMPANY_LOGO = '/JobHub/resources/img/logo/HDKoreaShipbuilding&OffshoreEngineeringCo.svg'
+where company_number = 61;
+
+UPDATE COMPANY
+SET COMPANY_LOGO = '/JobHub/resources/img/logo/COWAYCo.svg'
+where company_number = 62;
+
+UPDATE COMPANY
+SET COMPANY_LOGO = '/JobHub/resources/img/logo/HanwhaLife.svg'
+where company_number = 63;
+
+UPDATE COMPANY
+SET COMPANY_LOGO = '/JobHub/resources/img/logo/HyundaiEngineering&ConstructionCo.svg'
+where company_number = 64;
+
+UPDATE COMPANY
+SET COMPANY_LOGO = '/JobHub/resources/img/logo/JBFinancialGroup.svg'
+where company_number = 65;
+
+UPDATE COMPANY
+SET COMPANY_LOGO = '/JobHub/resources/img/logo/NHInvestment&Ssecurities.svg'
+where company_number = 66;
+
+UPDATE COMPANY
+SET COMPANY_LOGO = '/JobHub/resources/img/logo/KGETSCO.svg'
+where company_number = 67;
+
+UPDATE COMPANY
+SET COMPANY_LOGO = '/JobHub/resources/img/logo/SamsungEngineeringCo.svg'
+where company_number = 68;
+
+UPDATE COMPANY
+SET COMPANY_LOGO = '/JobHub/resources/img/logo/HanwhaAerospace.svg'
+where company_number = 69;
+
+UPDATE COMPANY
+SET COMPANY_LOGO = '/JobHub/resources/img/logo/BNKFinancialGroup.svg'
+where company_number = 70;
+
+UPDATE COMPANY
+SET COMPANY_LOGO = '/JobHub/resources/img/logo/HYOSUNGTNC.svg'
+where company_number = 71;
+
+UPDATE COMPANY
+SET COMPANY_LOGO = '/JobHub/resources/img/logo/POSCOInternational.svg'
+where company_number = 72;
+
+UPDATE COMPANY
+SET COMPANY_LOGO = '/JobHub/resources/img/logo/Hanwha.svg'
+where company_number = 73;
+
+UPDATE COMPANY
+SET COMPANY_LOGO = '/JobHub/resources/img/logo/L&FCO.svg'
+where company_number = 74;
+
+UPDATE COMPANY
+SET COMPANY_LOGO = '/JobHub/resources/img/logo/SKGasCo.svg'
+where company_number = 75;
+
+UPDATE COMPANY
+SET COMPANY_LOGO = '/JobHub/resources/img/logo/CJCheilJedangCorporation.svg'
+where company_number = 76;
+
+UPDATE COMPANY
+SET COMPANY_LOGO = '/JobHub/resources/img/logo/HankookTire&Technology.svg'
+where company_number = 77;
+
+UPDATE COMPANY
+SET COMPANY_LOGO = '/JobHub/resources/img/logo/KakaoBank.svg'
+where company_number = 78;
+
+UPDATE COMPANY
+SET COMPANY_LOGO = '/JobHub/resources/img/logo/DaewooEngineering&ConstructionCo.svg'
+where company_number = 79;
+
+UPDATE COMPANY
+SET COMPANY_LOGO = '/JobHub/resources/img/logo/DLE&CCo.svg'
+where company_number = 80;
+
+COMMIT;
